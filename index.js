f3ddcd206bead7196db03293eb2aa5414e98e88c
Piyush <piyushbansal941@gmail.com>
2022-12-21 17:41:51 +0530

    commit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/",(req, res)=>{
 const server=http.createServer(app);
 const io=socketIO(server, {
     cors: {
-      origin: "*",
+      origin: "http://localhost:3000",
     }
   });
 var users=[];
